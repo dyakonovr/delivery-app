0de556126d5e0d507dfea2f7e33d24409bb9c880
dyakonovr <dyakonov.rr@bk.ru>
2024-08-22 16:00:15 +0400

    чистый проект

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import stylisticJs from "@stylistic/eslint-plugin-js";
+import importX from "eslint-plugin-import-x";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
+  },
+  {
+    languageOptions: { globals: globals.browser }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
+
+  {
+    plugins: {
+      "@stylistic/js": stylisticJs,
+      "import-x": importX
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      semi: ["warn", "always"],
+      "comma-dangle": ["warn", "never"],
+      "@stylistic/js/comma-dangle": ["warn", "never"],
+      "jsx-quotes": ["warn", "prefer-double"],
+      "no-console": "warn",
+      "import-x/order": [
+        "warn",
+        {
+          groups: [
+            "external",
+            "builtin",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type"
+          ]
+        }
+      ]
+    }
+  }
+];
