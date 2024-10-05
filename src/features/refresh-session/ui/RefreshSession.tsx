ea76467a0deb9d6ecadfeb7cdf444d2a91b213f4
dyakonovr <dyakonov.rr@bk.ru>
2024-10-05 19:01:45 +0400

    перенес refresh-token, private-route в features (part 2 (wtf?))

---
+++ src/features/refresh-session/ui/RefreshSession.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useUserStore } from "@/entities/user";
+import { useSessionQuery } from "@/shared/api";
+import { GRAPHQL_AUTHORIZATION_CONTEXT } from "@/shared/config";
+
+export function RefreshSession() {
+  const { setUser } = useUserStore();
+  const { data, loading } = useSessionQuery({
+    ...GRAPHQL_AUTHORIZATION_CONTEXT
+  });
+
+  useEffect(() => {
+    if (loading || !data) return;
+    setUser(data.session.success ? data.session.user : null);
+  }, [data, loading]);
+
+  return null;
+}
